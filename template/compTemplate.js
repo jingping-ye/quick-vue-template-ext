7ed2b953bde124e8287d44124b695425f6675549
jingping.ye@kingstarfintech.com <1104639669@qq.com>
2020-10-23 15:56:07 +0800

    fix: 修复vue组件生成时组件名应该为大驼峰、顶级class应该为连字符的问题

--- template/compTemplate.js
+++ template/compTemplate.js
@@ -2,11 +2,36 @@
  * 组件模板
  */
 const fullVueComp = (componentName) => {
-  const dashComponentName = componentName.replace(/[A-Z]/g, function (val) {
-    return `-${val.toLowerCase()}`;
-  });
+  /**
+   * 获取组件名称
+   * @param {String} str 初始组件名称
+   * @note 大驼峰法
+   */
+  const getNameComp = function (str) {
+    return str.replace(str[0], function (val) {
+      return val.toUpperCase();
+    });
+  };
+
+  /***
+   * 获取类名称
+   * @note 连字符法
+   */
+  const getNameClass = function (str) {
+    return str.replace(/[A-Z]/g, function (val, index) {
+      if (index !== 0) {
+        return `-${val.toLowerCase()}`;
+      } else {
+        return val.toLowerCase();
+      }
+    });
+  };
+
+  const compName = getNameComp(componentName);
+  const className = getNameClass(componentName);
+
   const templateFile = `<template>
-  <div class="${componentName}">
+  <div class="${className}">
     ${componentName}组件
   </div>
 </template>
@@ -17,7 +42,7 @@ const value = {msg:"Hello World!"};
 //  引入外部资源
 // import someThing from 'SomeThing';
 export default {
-  name: '${componentName}',
+  name: '${compName}',
   mixins:[],
   components:{},
   props:{},
@@ -44,8 +69,8 @@ export default {
   destoryed(){}
 }
 </script>
-<style scoped>
-.${dashComponentName} {
+<style lang="scss" scoped>
+.${className} {
 
 }
 </style>`;
